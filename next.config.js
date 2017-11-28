75b8974ae449f0c918f3e3962330e364296599c3
Keisuke Kan <9renpoto@gmail.com>
2017-11-28 23:36:56 +0900

    initial commit

---
+++ next.config.js
@@ -0,0 +1,45 @@
+const { default: config } = require('nextein/config')
+const { entries, inCategory } = require('nextein/posts')
+
+const getStories = async () => {
+  const category = 'stories'
+  const all = await entries()
+  return all
+    .filter(inCategory(category, { includeSubCategories: true }))
+    .map(post => post.data.category)
+    .filter((category, idx, arr) => arr.indexOf(category) === idx)
+    .reduce(
+      (prev, entry) => ({
+        ...prev,
+        [`/${entry.replace(`${category}/`, '')}`]: {
+          page: '/stories',
+          query: { category: `${entry}` }
+        }
+      }),
+      {}
+    )
+}
+
+module.exports = config({
+  webpack: function (c, { dev }) {
+    if (dev) {
+      return c
+    }
+
+    c.resolve.alias = {
+      react: 'preact-compat/dist/preact-compat',
+      'react-dom': 'preact-compat/dist/preact-compat'
+    }
+
+    return c
+  },
+  exportPathMap: async () => {
+    const stories = await getStories()
+    return {
+      '/all-posts': { page: '/all-posts' },
+      '/sub-section': { page: '/sub-section' },
+      '/tags': { page: '/tags' },
+      ...stories
+    }
+  }
+})
