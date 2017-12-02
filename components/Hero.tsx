0deff88dd58f9b503323eec4fdb541e36bdf74b8
Keisuke Kan <9renpoto@gmail.com>
2017-12-03 00:21:09 +0900

    feat(posts) Added first entry

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 
 export default function Hello ({ title }) {
   return (
-    <section>
-      <h1>
-        <span>Hello, there. I'm </span>Nextein
-      </h1>
-      {!title && (
-        <p>
-          {' '}
-          A Blog / Static site generator based in <strong>Next.js</strong>
-        </p>
-      )}
-      {title && <h2>{title}</h2>}
+    <section className='hero is-primary'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>{title}</h1>
+        </div>
+      </div>
     </section>
   )
 }
