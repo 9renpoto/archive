13fbdcd05e8381167180358b38add4ca5fd373b7
Keisuke Kan <9renpoto@gmail.com>
2017-12-10 17:35:25 +0900

    fix(deploy) fix dirs

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init
 git remote add blog git@github.com:9renpoto/9renpoto.github.io.git
 git pull blog master
 cd ..
-find public -not -iwholename '*.git/*' | grep -v public/.git | xargs rm
+find out -not -iwholename '*.git/*' | grep -v out/.git | xargs rm
 npm run export
 cd out
 git add -A
