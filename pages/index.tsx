487bd1b02fc7b539d0975a3d17b2b852485430c2
Keisuke Kan <9renpoto@gmail.com>
2018-01-21 15:06:42 +0900

    chore(pages) Added calendar  (#52)
    * chore(pages) Added calendar
    
    * fix(pages) cast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-
+import * as fecha from 'fecha'
 import Head from 'next/head'
 import withPosts, { sortByDate } from 'nextein/posts'
+import React from 'react'
+import CalendarHeatmap from 'react-calendar-heatmap'
 
 import Layout from '../components/Layout'
 import Nav from '../components/Nav'
 import PostListEntry from '../components/PostListEntry'
 
+fecha.masks.postTime = 'YYYY-MM-DD'
+
 const Index = ({ posts }: any) => {
   posts.sort(sortByDate)
+
+  const rows = {}
+  posts.map(({ data: { date } }) => {
+    const s: any = fecha.parse(date, 'YYYY-MM-DDTHH:mm:ss.ZZ')
+    const d = fecha.format(s, 'postTime')
+    rows[d] = rows[d] || 0
+    rows[d]++
+  })
+
+  const today = new Date()
 
   return (
     <Layout>
@@ -17,6 +30,14 @@ const Index = ({ posts }: any) => {
         <link rel='stylesheet' href='/static/bundle.css' />
       </Head>
       <Nav />
+      <CalendarHeatmap
+        startDate={
+          new Date(today.getFullYear() - 1, today.getMonth(), today.getDay())
+        }
+        endDate={new Date()}
+        values={Object.keys(rows).map(date => ({ count: rows[date], date }))}
+        classForValue={value => `color-github-${value ? value.count : 0}`}
+      />
       <section>
         {posts.map((post: any, i: number) => (
           <PostListEntry key={i} {...post} />
