b02c576616a7a112f61b766cb6141ac1f1a2f19d
Keisuke Kan <9renpoto@gmail.com>
2018-02-23 00:33:14 +0900

    fix(deploy) reset --hard head

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm run clean
 mkdir -p out && cd out
 git init
 git remote add blog git@github.com:9renpoto/9renpoto.github.io.git
-git pull blog master
+git reset --hard blog/master
 cd ..
 find out -not -iwholename '*.git/*' | grep -v out/.git | xargs rm
 npm run export
