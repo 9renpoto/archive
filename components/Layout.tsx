91de8937227ca0ec0b7b3889eea6d16fde93eb4a
Keisuke Kan <9renpoto@gmail.com>
2018-03-10 12:04:20 +0900

    feat(packages) Added raven-js (#104)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,23 @@
+import * as Raven from 'raven-js'
 import React, { PureComponent } from 'react'
 import { initGA, logPageView } from '../utils/analytics'
 
-export default class Layout extends PureComponent {
+type Props = {}
+
+export default class Layout extends PureComponent<Props, { error: any }> {
+  constructor (props: Props) {
+    super(props)
+    Raven.config(
+      'https://f618bc349b614299939433b00e4be281@sentry.io/103445'
+    ).install()
+    this.state = { error: null }
+  }
+
+  componentDidCatch (error, errorInfo) {
+    this.setState({ error })
+    Raven.captureException(error, { extra: errorInfo })
+  }
+
   componentDidMount () {
     const w: any = window
     if (!w.GA_INITIALIZED) {
@@ -10,7 +26,20 @@ export default class Layout extends PureComponent {
     }
     logPageView()
   }
+
   render () {
-    return <div>{this.props.children}</div>
+    if (this.state.error) {
+      return (
+        <div
+          className='snap'
+          onClick={() => Raven.lastEventId() && Raven.showReportDialog()}
+        >
+          <img src={'https://gph.is/2d6qsYy'} />
+          <p>We're sorry — something's gone wrong.</p>
+          <p>Our team has been notified, but click here fill out a report.</p>
+        </div>
+      )
+    }
+    return this.props.children
   }
 }
