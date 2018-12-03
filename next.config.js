eeb527f9ddcc0848a24c58254b787a6370fb8033
Keisuke Kan <9renpoto@gmail.com>
2018-12-03 16:59:56 +0900

    fix(next.config) update nextein config (#387)

--- next.config.js
+++ next.config.js
@@ -1,33 +1,5 @@
-const config = require('nextein/config').default
+const { withNextein } = require('nextein/config')
 const withTypescript = require('@zeit/next-typescript')
 const withOffline = require('next-offline')
-const { entries, inCategory } = require('nextein/posts')
 
-const getStories = async () => {
-  const category = 'stories'
-  const all = await entries()
-  return all
-    .filter(inCategory(category, { includeSubCategories: true }))
-    .map(post => post.data.category)
-    .filter((category, idx, arr) => arr.indexOf(category) === idx)
-    .reduce(
-      (prev, entry) => ({
-        ...prev,
-        [`/${entry.replace(`${category}/`, '')}`]: {
-          page: '/stories',
-          query: { category: `${entry}` }
-        }
-      }),
-      {}
-    )
-}
-
-module.exports = config({
-  ...withOffline(withTypescript()),
-  exportPathMap: async () => {
-    const stories = await getStories()
-    return {
-      ...stories
-    }
-  }
-})
+module.exports = withOffline(withNextein(withTypescript()))
